c2130bb8c2632e75e80700ec7bb7b0c53ada4a5e
= <=>
2020-09-19 23:30:52 -0500

    process.env.PORT

--- app.js
+++ app.js
@@ -80,6 +80,8 @@ app.get("/words/list/random/:letter", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Listening to the port 3000");
 });
